89adc1891c6d385bdb23ee21511ba045d1b204ca
J. David Giese <johndgiese@gmail.com>
2015-02-21 16:47:40 -0500

    Prune weight module

--- weight.py
+++ weight.py
@@ -1,10 +1,4 @@
 import random
-import chess
-
-
-def fresh_board():
-    """Return a fresh chess board; white to move."""
-    return chess.Bitboard()
 
 
 def weight(board):
@@ -12,12 +6,3 @@ def weight(board):
     return random.choice([1.0, 2.0, 3.0])  # TODO: implement
 
 
-def next_boards(board):
-    """
-    Return a generator for producing possible next moves from a given
-    board.
-    """
-    for m in board.legal_moves:
-        board.push(m)
-        yield chess.Bitboard(board.fen())  # create new board object
-        board.pop()
