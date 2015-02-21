0caa90dd8197a6720067dac6df2d2ec761bf22b8
Daniel Hensley <hensley.daniel@gmail.com>
2015-02-21 13:17:49 -0800

    Add basic ai module.

---
+++ ai.py
@@ -0,0 +1,86 @@
+## Authors: Daniel Hensley and Ryan Orendorff
+
+from cytoolz import partial
+
+## Global definition of infinity.
+inf = float('inf')
+
+def adversarial_search(prune=False, horizon=3, value):
+    if prune:
+        return (partial(min_value_prune, horizon, value),
+                partial(max_value_prune, horizon, value))
+    else:
+        return (partial(min_value, horizon, value),
+                partial(max_value, horizon, value))
+
+
+def min_value(horizon, value, state):
+    ''' Find next with minimum value function.
+
+    Parameters
+    ----------
+    horizon: int
+        Number of future steps to terminal condition of value function.
+    state: object
+        Object describing (game) state.
+    value: function
+        Function that returns the value of a (game) state.
+
+    Returns
+    -------
+    object
+        The optimal action to be taken.
+
+    Examples
+    --------
+        TODO: fill in.
+    '''
+    if horizon == 0:
+        return value(state)
+
+    v = inf
+    for a in actions(state):
+        next_state = a(state)
+        v = min(v, max_value(horizon - 1,  value, next_state))
+
+    return v
+
+
+def max_value(horizon, value, state):
+    ''' Find next with minimum value function.
+
+    Parameters
+    ----------
+    horizon: int
+        Number of future steps to terminal condition of value function.
+    state: object
+        Object describing (game) state.
+    value: function
+        Function that returns the value of a (game) state.
+
+    Returns
+    -------
+    object
+        The optimal action to be taken.
+
+    Examples
+    --------
+        TODO: fill in.
+    '''
+    if horizon == 0:
+        return value(state)
+
+    v = -inf
+    for a in actions(state):
+        next_state = a(state)
+        v = min(v, min_value(horizon - 1, value, next_state)
+
+    return v
+
+
+def min_value_prune(horizon, state, value):
+    pass
+
+
+def max_value_prune(horizon, state, value):
+    pass
