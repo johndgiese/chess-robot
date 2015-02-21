86f43d26c96b8fda09dd51e6b8f9704686907d2f
J. David Giese <johndgiese@gmail.com>
2015-02-21 15:45:08 -0500

    Implement fresh_board and board generator

--- weight.py
+++ weight.py
@@ -1,9 +1,10 @@
 import random
+import chess
 
 
 def fresh_board():
     """Return a fresh chess board; white to move."""
-    return {}  # TODO: implement
+    return chess.Bitboard()
 
 
 def weight(board):
@@ -12,5 +13,11 @@ def weight(board):
 
 
 def next_boards(board):
-    """Return a generator for producing possible next moves from a given board."""
-    return [{}, {}, {}]  # TODO: implement
+    """
+    Return a generator for producing possible next moves from a given
+    board.
+    """
+    for m in board.legal_moves:
+        board.push(m)
+        yield chess.Bitboard(board.fen())  # create new board object
+        board.pop()
