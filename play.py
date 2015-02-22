e4d1a398e6e94b672ae6202c9fe830cf0a1af3fb
J. David Giese <johndgiese@gmail.com>
2015-02-22 04:10:32 -0500

    Update play algorithms

--- play.py
+++ play.py
@@ -4,6 +4,6 @@ from util import play_game, possible_actions, generate_move_function
 
 if __name__ == "__main__":
     white = generate_move_function(weight, 3)
-    black = generate_move_function(random_weight, 3)
+    black = generate_move_function(weight, 2)
     play_game(white, black, display=True)
 
